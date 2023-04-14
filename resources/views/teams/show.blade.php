4bff507b375610765d417e935fe68731970727d0
MazBaz <mrlog42@gmail.com>
2023-04-14 09:42:02 +0200

    Fix permission

--- resources/views/teams/show.blade.php
+++ resources/views/teams/show.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
             @livewire('teams.update-team-name-form', ['team' => $team])
 
-            @if(!$team->personal_team || Auth::user()->hasTeamPermission($team, "create_invite"))
+            @if(!$team->personal_team && Auth::user()->hasTeamPermission($team, "create_invite"))
                 @livewire('teams.team-member-manager', ['team' => $team])
             @endif
 
