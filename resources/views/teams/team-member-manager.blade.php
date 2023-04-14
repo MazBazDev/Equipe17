5d05d78b5d8785693a53f7356b85b0499701e26d
MazBaz <mrlog42@gmail.com>
2023-04-14 10:22:00 +0200

    Update front

--- resources/views/teams/team-member-manager.blade.php
+++ resources/views/teams/team-member-manager.blade.php
@@ -18,10 +18,8 @@
                 <div class="col-span-6 lg:col-span-4 px-4 py-5 bg-white dark:bg-gray-800 sm:p-6 shadow sm:rounded-tl-md sm:rounded-tr-md mt-5">
                     <form action="{{ route('invite.create', $team) }}" method="post">
                         @csrf
-                        
-                        <div class="mt-6">
-                            <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">Create / View link</button>
-                        </div>
+
+                        <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">Create / View link</button>
                     </form>
                 </div>
         </div>
